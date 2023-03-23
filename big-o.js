e2f8661e5efc6992a976d5dd4d2abcda1e518258
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-03-23 17:38:40 -0600

    Added 1 more exercise

--- big-o.js
+++ big-o.js
@@ -20,3 +20,25 @@ console.log(funChallenge([1, 2, 3, 4]));
  ** The loop itself is O(n) because is relying on the size of our input parameter. As the input increases, the number of operations will increase as well.
  ** Additionally, anything within the loop will be O(n) because it depends on the input
  */
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+  let a = 5; // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
+
+  for (let i = 0; i < input; i++) {
+    // O(n)
+    let x = i + 1; // O(n)
+    let y = i + 2; // O(n)
+    let z = i + 3; // O(n)
+  }
+  for (let j = 0; j < input; j++) {
+    // O(n)
+    let p = j * 2; // O(n)
+    let q = j * 2; // O(n)
+  }
+  let whoAmI = "I don't know"; // O(1)
+}
+
+// The big O of the entire function is O(n)
